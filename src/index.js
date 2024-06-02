c27690f2e65c16d69503a8bf9d2f39beb89367d9
sen98ia <sen98ia@gmail.com>
2024-06-02 17:55:32 +0300

    add src/idex.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const countRound = 3;
+
+const gameRun = (gameRule, getCorrectAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log(gameRule);
+
+  for (let i = 0; i < countRound; i += 1) {
+    const [gameNumber, rightAnswer] = getCorrectAnswer();
+    console.log(`Question: ${gameNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  }
+};
+
+export default gameRun;
