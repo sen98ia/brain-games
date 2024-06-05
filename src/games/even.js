ab33ca0404d44e8b434024d943963185c59f44b0
sen98ia <sen98ia@gmail.com>
2024-06-05 14:54:18 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,9 +6,9 @@ const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const getCorrectAnswer = () => {
-  const gameNumber = randomNumberGenerator();
-  const rightAnswer = isEven(gameNumber) ? 'yes' : 'no';
-  return [gameNumber, rightAnswer];
+  const givenNumber = randomNumberGenerator();
+  const rightAnswer = isEven(givenNumber) ? 'yes' : 'no';
+  return [givenNumber, rightAnswer];
 };
 
 export default () => gameRun(gameRule, getCorrectAnswer);
