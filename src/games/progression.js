3b0d3252ff16454015d76ee940d0e940db0de2a3
sen98ia <sen98ia@gmail.com>
2024-06-05 15:30:13 +0300

    add src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import gameRun from '../index.js';
+import randomNumberGenerator from '../num.generator.js';
+
+const gameRule = 'What number is missing in the progression?';
+
+const progressionGenerator = () => {
+  const randomProgression = [];
+  let progressionNumber = randomNumberGenerator(1, 50);
+  randomProgression.push(progressionNumber);
+  const progressionLength = randomNumberGenerator(5, 10);
+  const progressionStep = randomNumberGenerator(1, 20);
+  for (let i = 0; i < progressionLength - 1; i += 1) {
+    progressionNumber += progressionStep;
+    randomProgression.push(progressionNumber);
+  }
+  return randomProgression;
+};
+
+const getCorrectAnswer = () => {
+  const givenProgression = progressionGenerator();
+  const randomIndex = randomNumberGenerator(0, givenProgression.length - 1);
+  const rightAnswer = givenProgression[randomIndex];
+  givenProgression[randomIndex] = '..';
+  return [givenProgression, String(rightAnswer)];
+};
+
+export default () => gameRun(gameRule, getCorrectAnswer);
